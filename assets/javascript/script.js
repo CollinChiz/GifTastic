ce6fa68bfaa26c81428190268d5226f9472da576
chizzy <cwchiz16@gmail.com>
2019-09-22 16:00:05 -0600

    creating functions

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,10 +1,32 @@
+$(document).ready(function() {
+
+
+var sports = ["football", "basketball", "baseball", "golf", "soccer", "rugby", "tennis", "volleyball", "track and field", "bowling", "cricket", "handball", "kickball"]
+function displayButtons() {
+    for(i=0; i<sports.length; i++) {
+    var buttons =  $("<button>" + sports[i] + "</button>")
+    $(buttons).attr("id", "button");
+  
+    $("#buttons").append(buttons);
+    }
+}
+
+displayButtons();
+
+function addButton() {
+    $(document).on("click", "#search", function(e) {
+        e.preventDefault();
+        var text = $("#textBox").val();
+        console.log(text);
+    });
+}
+
 var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=fR85SEr9y3LQVsH8pxtvo8PoeNQXrSLA&limit=10&rating=PG"
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function (response){
-    console.log(response);
-    $("body").append(`<p>${response}</p>`);
-
     
+    
+})
 })
