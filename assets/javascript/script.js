27d9d573801a8a75c2b228c2fa9debdca9074a3a
chizzy <cwchiz16@gmail.com>
2019-09-25 15:46:33 -0600

    continuted development

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,31 +2,52 @@ $(document).ready(function() {
 
 
 var sports = ["football", "basketball", "baseball", "golf", "soccer", "rugby", "tennis", "volleyball", "track and field", "bowling", "cricket", "handball", "kickball"]
+
 function displayButtons() {
+    $("#buttons").empty();
     for(i=0; i<sports.length; i++) {
     var buttons =  $("<button>" + sports[i] + "</button>")
-    $(buttons).attr("id", "button");
-  
+    $(buttons).attr("class", "button");
+    
+    $(buttons).attr("data-name", sports[i]);
+    
+
+    
     $("#buttons").append(buttons);
     }
 }
+// button click
+$(document).on("click", ".button", function() {
+  var value = $(this).attr("data-name");
+  console.log(value)
+  var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=fR85SEr9y3LQVsH8pxtvo8PoeNQXrSLA&q=" + value + "&limit=10&offset=0&rating=PG&lang=en"
+  $.ajax({
+      url: queryURL,
+      method: "GET"
+  }).then(function (response){
+      console.log(response);
+      for(i=0; i<response.data.length; i++) {
+          console.log(response.data[i]);
+          var gif = response.data[i].url
+            var image = $("<img/>").attr("src", gif)
+            $("#gifs").append(image)    
+      }
+  })
+
+})
+
+
+
+    $(document).on("click", "#search", function(event) {
+        event.preventDefault();
+        var text = $("#textBox").val();
+        sports.push(text)
+        
+        displayButtons();
+    });
+
 
 displayButtons();
 
-function addButton() {
-    $(document).on("click", "#search", function(e) {
-        e.preventDefault();
-        var text = $("#textBox").val();
-        console.log(text);
-    });
-}
+});
 
-var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=fR85SEr9y3LQVsH8pxtvo8PoeNQXrSLA&limit=10&rating=PG"
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response){
-    
-    
-})
-})
