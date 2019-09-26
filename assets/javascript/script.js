2b1ddd712a16e706a47c615526252d124051d68f
chizzy <cwchiz16@gmail.com>
2019-09-25 20:40:42 -0600

    pretty much done

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 
-var sports = ["football", "basketball", "baseball", "golf", "soccer", "rugby", "tennis", "volleyball", "track and field", "bowling", "cricket", "handball", "kickball"]
+var sports = ["football", "basketball", "baseball", "golf", "soccer", "rugby", "tennis", "volleyball", "track and field", "bowling"]
 
 function displayButtons() {
     $("#buttons").empty();
@@ -27,27 +27,46 @@ $(document).on("click", ".button", function() {
   }).then(function (response){
       console.log(response);
       for( var i=0; i<response.data.length; i++) {
-          console.log(response.data[i].bitly_gif_url);
-          var gif = response.data[i].bitly_gif_url
+          console.log(response.data[i].images.fixed_height);
+          var gif = response.data[i].images.fixed_height_still.url
             var image = $("<img>").attr("src", gif)
+            image.attr("data-still", response.data[i].images.fixed_height_still.url)
+            image.attr("data-animate", response.data[i].images.fixed_height.url)
+            image.attr("data-state", "still")
+            image.attr("class", "gif")
             $("#gifs").append(image)    
       }
-  })
-
+    })    
+    
 })
+$(document).on("click", ".gif", function() {
 
+    var state = $(this).attr("data-state");
+    console.log(state)
+    
+    if(state == "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+})
+    
 
-
-    $(document).on("click", "#search", function(event) {
-        event.preventDefault();
-        var text = $("#textBox").val();
-        sports.push(text)
-        
-        displayButtons();
-    });
+$(document).on("click", "#add", function(event) {
+    event.preventDefault();
+    var text = $("#textBox").val();
+    sports.push(text)
+    displayButtons();
+    $("#textBox").val().empty();
+});
 
 
 displayButtons();
+ })
 
-});
+
+
+
 
